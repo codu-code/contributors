552aee3fb7209d44c2ec88805b73996fd9c042ea
dreplica <adejo.david@aiesec.net>
2020-04-14 19:09:51 +0100

    added dreplica to contributors

--- src/contributors.js
+++ src/contributors.js
@@ -1,10 +1,18 @@
 export default [
-    {
-        id: "1123abc",
-        name: "Niall Maher",
-        title: "JavaScript Developer",
-        username: "NiallJoeMaher",
-        website: "www.codu.ie",
-        skills: ["JavaScript", "CSS", "HTML", "NodeJS", "MongoDB", "AWS"]
-    }
+	{
+		id: '1123abc',
+		name: 'Niall Maher',
+		title: 'JavaScript Developer',
+		username: 'NiallJoeMaher',
+		website: 'www.codu.ie',
+		skills: [ 'JavaScript', 'CSS', 'HTML', 'NodeJS', 'MongoDB', 'AWS' ]
+	},
+	{
+		id: '234',
+		name: 'Adejo MaDavidher',
+		title: 'JavaScript Developer',
+		username: 'dreplica',
+		website: 'github.com/dreplica',
+		skills: [ 'JavaScript', 'React & RNative', 'CSS', 'HTML', 'NodeJS', 'MongoDB', 'AWS' ]
+	}
 ];
