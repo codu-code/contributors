5bf1176b1b8288a2d5b2db8a89e02ca997bc6ac2
Niall Maher <niall@codu.ie>
2020-04-14 19:03:39 +0100

    Updating title for Niall Maher

--- src/contributors.js
+++ src/contributors.js
@@ -2,7 +2,7 @@ export default [
     {
         id: "1123abc",
         name: "Niall Maher",
-        title: "JavaScript Developer",
+        title: "Mr. Codú Community",
         username: "NiallJoeMaher",
         website: "www.codu.ie",
         skills: ["JavaScript", "CSS", "HTML", "NodeJS", "MongoDB", "AWS"]
